f21b0442e3f4c1131f0faa5a5b3a83464ac06082
anandasaia <anandu251@gmail.com>
2024-02-18 14:46:12 +0530

    render docs

--- notetaking_api/settings.py
+++ notetaking_api/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import dj_database_url
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'drf_yasg'
+    'dj_database_url'
 ]
 
 MIDDLEWARE = [
